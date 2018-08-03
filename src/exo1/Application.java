c677f452f6e049080ebaa597a0e33ab2579e8747
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-08-03 11:46:29 +0200

    getters, setters, and class Application

---
+++ src/exo1/Application.java
@@ -0,0 +1,12 @@
+package exo1;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ImplementInterface1 ex1 = new ImplementInterface1();
+		
+	}
+
+}
--- src/exo1/ImplementInterface1.java
+++ src/exo1/ImplementInterface1.java
@@ -14,4 +14,22 @@ public class ImplementInterface1 implements Exemple {
 	{
 		return(Math.max(v1, v2));
 	}
+
+	public int getV1() {
+		return v1;
+	}
+
+	public void setV1(int v1) {
+		this.v1 = v1;
+	}
+
+	public int getV2() {
+		return v2;
+	}
+
+	public void setV2(int v2) {
+		this.v2 = v2;
+	}
+	
+	
 }
