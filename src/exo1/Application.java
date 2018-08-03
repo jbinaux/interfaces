fdca29a64655dc65a85848e63169e17d33320c38
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-08-03 11:51:19 +0200

    display maximum

--- src/exo1/Application.java
+++ src/exo1/Application.java
@@ -6,11 +6,17 @@ public class Application {
 		// TODO Auto-generated method stub
 
 		ImplementInterface1 ex1 = new ImplementInterface1();
+		ImplementInterface2 ex2 = new ImplementInterface2();
 		
 		ex1.setV1(14);
 		ex1.setV2(12);
 		
+		ex2.setV1(165);
+		ex2.setV2(189);
+		ex2.setV3(171);
+		
 		System.out.println("La note minimale est : " + ex1.minimum());
+		System.out.println("la taille maximum est : " + ex2.maximum());
 	}
 
 }
